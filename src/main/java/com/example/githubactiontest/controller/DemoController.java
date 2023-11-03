2504687e7048f63a63b883953442783f7ed02722
rugii913 <rugii913@gmail.com>
2023-11-03 18:36:21 +0900

    dynamic variable 추가
    ㅁdeploy.yaml에 Set yaml file 스텝 추가
     ㅇ이 스텝에서 yaml 파일에 있는 특정 값을 대체(microsoft/variable-substitution 사용)
    ㅁapplication.yaml
     ㅇ동적으로 바꿔볼 값 설정함
    ㅁ DemoController
     ㅇ application.yaml에서 동적으로 바뀔 값을 응답으로 내보냄
      - 실제로 배포 과정에서 값이 대체되는지 확인

--- src/main/java/com/example/githubactiontest/controller/DemoController.java
+++ src/main/java/com/example/githubactiontest/controller/DemoController.java
@@ -1,13 +1,16 @@
 package com.example.githubactiontest.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
+    @Value("${override.value}") // application.yaml의 값을 가져옴
+    private String value;
 
     @GetMapping("/demo")
     public String demo() {
-        return "demo";
+        return "demo " + value;
     }
 }
